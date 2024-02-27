edcf5086f9bf1a22d568a3c1b40a5fd46a931daf
pedroserodio <serodiomg@gmail.com>
2024-02-27 17:11:27 -0300

    feat: FIrst Commit

---
+++ frontend/.angular/cache/17.1.3/vite/deps/@angular_material_button.js
@@ -0,0 +1,33 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-KKCPO5OZ.js";
+import "./chunk-HCJBMX6X.js";
+import "./chunk-VEJBGHSW.js";
+import "./chunk-VOB56UIL.js";
+import "./chunk-WKYGNSYM.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map
